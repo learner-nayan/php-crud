c55021db0de7cc10f70343bede3221855ae023c3
Nayan <nayan@Nayans-MacBook-Air.local>
2024-03-15 15:51:43 +0545

    Readme file

--- students/index.php
+++ students/index.php
@@ -6,14 +6,14 @@ try{
     echo "Error including: ".$e;
 }
 
-// $sql = "SELECT * FROM users";
+$sql = "SELECT * FROM users";
 
-// $data = $connection->query($sql);
+$data = $connection->query($sql);
 
-// if($data->num_rows>0){
-//     while($element=$data->fetch_assoc()){
-//         print_r($element);
-//     }
-// }
+if($data->num_rows>0){
+    while($element=$data->fetch_assoc()){
+        print_r($element);
+    }
+}
 
 echo "Hello world";
